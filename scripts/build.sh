c09bb29953298d1867e4abe712204736816b6db1
YooJuno <juno980220@naver.com>
2026-02-16 07:50:25 +0000

    chore(scripts): update systemd paths and build flow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,16 @@
-# !/bin/bash
+#!/bin/bash
+
+# build back-end jar
+cd /home/juno/Workspace/btc-auto-trader/apps/backend
+./gradlew bootJar
 
 # build front-end
 cd /home/juno/Workspace/btc-auto-trader/apps/frontend
 npm install
 npm run build
+
+sudo cp /home/juno/Workspace/btc-auto-trader/scripts/btc-backend.service /etc/systemd/system/btc-backend.service
+sudo cp /home/juno/Workspace/btc-auto-trader/scripts/btc-frontend.service /etc/systemd/system/btc-frontend.service
 
 sudo systemctl daemon-reload
 
